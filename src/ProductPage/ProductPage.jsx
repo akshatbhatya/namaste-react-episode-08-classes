b9b4b95d050e2767848c08111a131a86317c0aac
akshat <akshatbhatya@gmail.com>
2024-01-22 17:47:21 +0530

    che fetch api method

--- src/ProductPage/ProductPage.jsx
+++ src/ProductPage/ProductPage.jsx
@@ -5,35 +5,42 @@ import { Link } from "react-router-dom"
 let ProductPage = () => {
     let url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3260152&lng=75.57618289999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-    const [product,setProduct]=useState([])
+    const [product, setProduct] = useState([])
 
     useEffect(() => {
-        async function fetchData() {
-
-            let fetchData = await fetch(url);
-            let response = await fetchData.json();
-            let result=response.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
-            console.log(result);
-            let allProduct=[]
-            for(let i=0;i<result.length;i++){
-                allProduct.push(result[i].info)
-
-            }
-            console.log(allProduct);
-            setProduct(allProduct)
-        }
         fetchData()
     }, [])
 
-    
+    async function fetchData() {
+
+        try {
+            let fetchData = await fetch(url);
+            let response = await fetchData.json();
+            let result = response.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+            console.log(result);
+            let allProduct = []
+            for (let i = 0; i < result.length; i++) {
+                allProduct.push(result[i].info)
+
+            }
+            setProduct(allProduct)
+
+        } catch (error) {
+            console.log(error);
+
+        }
+
+    }
+
+
 
     return (
         <div className="productPage_parent">
-           {
-            product.map((item)=>{
-                return  <Link key={item.id} to={"/reasturant/"+item.id}><Product data={item} /></Link>
-            })
-           }
+            {
+                product.map((item) => {
+                    return <Link key={item.id} to={"/reasturant/" + item.id}><Product data={item} /></Link>
+                })
+            }
 
         </div>
     )
