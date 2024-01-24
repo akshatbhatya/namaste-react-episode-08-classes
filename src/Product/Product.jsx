8c84c0ddc6bb94c64080e7cac2ee518aeeeca627
akshat <akshatbhatya@gmail.com>
2024-01-24 23:15:41 +0530

    add tailwind

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -5,16 +5,16 @@ let Product = ({ data }) => {
     let { name, cloudinaryImageId, locality, areaName, avgRating } = data
 
     return (
-        <div className="inline-block bg-[#f0f0f0] p-4 rounded-md">
+        <div className="inline-block bg-[#f0f0f0] p-4 rounded-md h-[500px]">
             <div className="product_image">
                 <img src={cdn + cloudinaryImageId} className="w-[300px] rounded-sm h-[300px]" alt={cloudinaryImageId} />
 
             </div>
             <div className="mt-5">
-                <h4>{name}</h4>
+                <h4 className="font-bold w-[290px]">{name}</h4>
                 <h4>{locality}</h4>
                 <h4>{areaName}</h4>
-                <h4>{avgRating}</h4>
+                <h4 className="text-green-700">* {avgRating}</h4>
             </div>
         </div>
     )
