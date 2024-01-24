e816ed09df6937bc05e078c28c44efc563839636
akshat <akshatbhatya@gmail.com>
2024-01-24 17:11:49 +0530

    add Grosery component

---
+++ src/Grosery/Grosery.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+class Grosery extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return (
+            <>
+
+                <h5>Grosery page</h5>
+            </>
+        )
+    }
+}
+
+export default Grosery
