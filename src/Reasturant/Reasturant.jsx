520f6b591ab098cd5a2e051394e1d90c809219a2
akshat <akshatbhatya@gmail.com>
2024-01-25 16:03:05 +0530

    add styles

--- src/Reasturant/Reasturant.jsx
+++ src/Reasturant/Reasturant.jsx
@@ -1,25 +1,51 @@
+import React from 'react';
 import { useParams } from 'react-router'
 import ShemerPage from '../ShemmerPage/ShemmerPage'
 import useResturantmenu from '../CustomHooks/UseResturantmenu'
+import { useEffect } from 'react';
 
 function Reasturant() {
+
+  useEffect(()=>{
+    document.title="Product-Detail"
+
+    return ()=>{
+      document.title="Swiggy Store"
+    }
+  },[])
   const { resId } = useParams()
 
-  let product = useResturantmenu(resId)
+  let product = useResturantmenu(resId);
+  console.log(product[0]);
 
-  let { name, locality, cuisines } = product[0]?.card?.card?.info || {}
+  let { name, locality, cuisines,avgRating,cloudinaryImageId,city
+    ,costForTwoMessage
+  } = product[0]?.card?.card?.info || {}
+
+
+  let data = product[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards || [];
+  let category = data.filter((c) => c.card.card["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+  console.log(category);
+
+
 
   if (product.length == 0) {
     return <ShemerPage />
   }
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <h2>{locality}</h2>
+    <div className=' mt-20 mx-5 flex justify-center items-center flex-col'>
+
+      <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/${cloudinaryImageId}`} className="w-[300px] h-[300px] " alt="" />
+      <h2 className='font-bold my-6'>{name}</h2>
+      <h3 className={avgRating>4?"font-medium text-green-600":"font-medium text-yellow-400"}>* {avgRating}</h3>
+      <h3>{costForTwoMessage}</h3>
+      <h3>{locality}</h3>
+      <h2>{city}</h2>
       <h3>{cuisines}</h3>
+      
     </div>
   )
 }
 
-export default Reasturant
+export default Reasturant;
