a80b99ff6a4bdf1b21cb4e7174eaef3369315667
akshat <akshatbhatya@gmail.com>
2024-01-25 17:33:43 +0530

    tailwind is not working

---
+++ CategoryAcc/CategoryAcc.jsx
@@ -0,0 +1,12 @@
+let CategoryAcc=()=>{
+
+
+    return(
+        <div className="bg-slate-800">
+        <span className="text-red-600">heading</span>
+        </div>
+
+    )
+}
+
+export default CategoryAcc
--- src/Reasturant/Reasturant.jsx
+++ src/Reasturant/Reasturant.jsx
@@ -3,23 +3,24 @@ import { useParams } from 'react-router'
 import ShemerPage from '../ShemmerPage/ShemmerPage'
 import useResturantmenu from '../CustomHooks/UseResturantmenu'
 import { useEffect } from 'react';
+import CategoryAcc from '../../CategoryAcc/CategoryAcc';
 
 function Reasturant() {
 
-  useEffect(()=>{
-    document.title="Product-Detail"
+  useEffect(() => {
+    document.title = "Product-Detail"
 
-    return ()=>{
-      document.title="Swiggy Store"
+    return () => {
+      document.title = "Swiggy Store"
     }
-  },[])
+  }, [])
   const { resId } = useParams()
 
   let product = useResturantmenu(resId);
   console.log(product[0]);
 
-  let { name, locality, cuisines,avgRating,cloudinaryImageId,city
-    ,costForTwoMessage
+  let { name, locality, cuisines, avgRating, cloudinaryImageId, city
+    , costForTwoMessage
   } = product[0]?.card?.card?.info || {}
 
 
@@ -34,17 +35,22 @@ function Reasturant() {
   }
 
   return (
-    <div className=' mt-20 mx-5 inline-flex p-2 rounded justify-center  flex-col bg-[#f0f0f0] '>
 
-      <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/${cloudinaryImageId}`} className="w-[300px] h-[300px] " alt="" />
-      <h2 className='font-bold my-6'>{name}</h2>
-      <h3 className={avgRating>4?"font-medium text-green-600":"font-medium text-yellow-400"}>* {avgRating}</h3>
-      <h3 className='text-green-600'>{costForTwoMessage}</h3>
-      <h3>{locality}</h3>
-      <h2>{city}</h2>
-      <h3>{cuisines}</h3>
-      
-    </div>
+    <>
+      <div className=' mt-20 mx-5 flex p-2 rounded justify-center  flex-col bg-[#f0f0f0] '>
+
+        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/${cloudinaryImageId}`} className="w-[300px] h-[300px] " alt="" />
+        <h2 className='font-bold my-6'>{name}</h2>
+        <h3 className={avgRating > 4 ? "font-medium text-green-600" : "font-medium text-yellow-400"}>* {avgRating}</h3>
+        <h3 className='text-green-600'>{costForTwoMessage}</h3>
+        <h3>{locality}</h3>
+        <h2>{city}</h2>
+        <h3>{cuisines}</h3>
+
+      </div>
+
+      <CategoryAcc />
+    </>
   )
 }
 
