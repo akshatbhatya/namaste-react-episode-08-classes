e98de830228c4e6af1ad2fa1205f10ad6386e807
akshat <akshatbhatya@gmail.com>
2024-01-26 15:50:26 +0530

    add css to button

--- src/CategoryAcc/CategoryAcc.jsx
+++ src/CategoryAcc/CategoryAcc.jsx
@@ -1,12 +1,56 @@
-let CategoryAcc=({data})=>{
+import { useState } from "react";
+
+let CategoryAcc = ({ data }) => {
+
+
+    const [click, setClick] = useState(false)
 
     console.log(data);
 
-    return(
-        <div className="w-6/12 mx-auto my-5 flex justify-between bg-[#f0f0f0] p-5 rounded-sm shadow-2xl ">
-        <span className="text-red-600 font-bold">{data.card.card.title} ({data.card.card.itemCards.length})</span>
-        <span className="cursor-pointer font-bold">+</span>
-        </div>
+    return (
+        <>
+
+            <div className="w-6/12 mx-auto mt-5 flex justify-between bg-[#f0f0f0] p-5 rounded-sm shadow-2xl ">
+                <span className="text-red-600 font-bold">{data.card.card.title} ({data.card.card.itemCards.length})</span>
+                <button className="cursor-pointer font-bold" onClick={() => setClick
+                    (!click)}>{click ? '-' : "+"}</button>
+            </div>
+
+            <div className={click ? "w-6/12 mx-auto bg-[#f0f0f0] p-5 rounded-sm shadow-2xl mb-5" : "hidden"}>
+                {data?.card?.card?.itemCards?.map((item, index) => <div key={index} className="list-none"><>
+                    <br />
+
+
+
+
+                    <div className="flex justify-between items-center">
+
+                        <div className="content">
+                            <h4>{item?.card?.info?.name}</h4>
+                            <h4>{item?.card?.info?.description}</h4>
+                            <h4>RS {item?.card?.info?.defaultPrice / 100 || item?.card?.info?.price / 100}</h4>
+                            <h4>{item?.card?.info?.category}</h4>
+                        </div>
+
+                        <div className="image">
+                            <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/${item?.card?.info?.imageId}`} className="w-[150px] h-[150px]" alt="photo" />
+
+                            <div>
+                                <button className="bg-slate-500 px-3 rounded text-white" >Add</button>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+
+
+                    <br />
+                    <hr />
+                </>
+                </div>)}
+            </div>
+            <br />
+
+        </>
 
     )
 }
