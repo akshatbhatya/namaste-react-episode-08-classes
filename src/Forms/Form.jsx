28b2a7d96f5b175d9926a89c560e92b497c1bfab
akshat <akshatbhatya@gmail.com>
2024-01-29 18:25:16 +0530

    configure form

--- src/Forms/Form.jsx
+++ src/Forms/Form.jsx
@@ -1,41 +1,94 @@
+import { name } from "commander";
+import React from "react";
+import { useState } from "react";
 
 const Forms = () => {
+
+
+    let intialData={
+        username:"",
+        email:"",
+        password:"",
+        conformPassword:""
+
+
+
+    }
+
+    let [inputValue,setInputValue]=useState(intialData)
+    let handleSubmit=(e)=>{
+        e.preventDefault()
+        return <h1>form submitted</h1>
+    }
+
+    let handlChange=(e)=>{
+
+        let target=e.target
+        setInputValue((prev)=>({...prev,[target.name]:target.value}))
+        console.log(inputValue);
+
+    }
     return (
         <div className="bg-slate-400 flex justify-center items-center flex-col w-[400px] mt-10 mx-auto p-6 rounded-md">
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                 <h2 className="text-white capitalize font-bold">Login Form </h2>
                 <hr />
-                <label className="text-white m-0 capitalize font-medium">Username</label>
-                <input
+                <label className="text-white m-0 capitalize font-medium">Username
+                    <br />
+                    <input
 
-                    type="text"
-                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 m-0 w-[300px]"
-                    placeholder="UserName"
-                    name="username"
-                />
-                <label className="text-white m-0 capitalize font-medium">email</label>
-                <input
-                    type="text"
-                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                    placeholder="Email"
-                    name="email"
-                />
-                <label className="text-white m-0 capitalize font-medium">password</label>
-                <input
-                    type="text"
-                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                    placeholder="Password"
-                    name="password"
-                />
+                        type="text"
+                        className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 m-0 w-[300px]  text-black"
+                        placeholder="UserName"
+                        name="username"
+                        required
+                        pattern="^[A-Za-z]{4,14}$"
+                        value={inputValue.username}
+                        onChange={handlChange}
+                        aria-errormessage="username btween A-Z a-z till 16 aplhabet"
+                    />
+                    <br />
+                    <span>{inputValue.username.length<3?"requre least 4 character":"correct"}</span>
+                </label>
+                <label className="text-white m-0 capitalize font-medium">email
+                    <br />
+                    <input
+                        type="email"
+                        className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 w-[300px]  text-black"
+                        placeholder="Email"
+                        name="email"
+                        required
+                        value={inputValue.email}
+                        onChange={handlChange}
+                    />
+                </label>
+                <label className="text-white m-0 capitalize font-medium">password
+                    <br />
+                    <input
+                        type="password"
+                        className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 w-[300px]  text-black"
+                        placeholder="Password"
+                        name="password"
+                        required
+                        value={inputValue.password}
+                        onChange={handlChange}
+                    />
+                </label>
 
-                <label className="text-white m-0 capitalize font-medium">confirm password</label>
+                <label className="text-white m-0 capitalize font-medium">confirm password
+                    <br />
 
-                <input
-                    type="text"
-                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                    placeholder="Confirm Password"
-                    name="conformPassword"
-                />
+                    <input
+                        type="password"
+                        className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 w-[300px] text-black"
+                        placeholder="Confirm Password"
+                        name="conformPassword"
+                        required
+                        value={inputValue.conformPassword}
+                        onChange={handlChange}
+                        
+                    />
+                </label>
 
                 <button
                     type="submit"
@@ -49,3 +102,5 @@ const Forms = () => {
 };
 
 export default Forms;
+
+
